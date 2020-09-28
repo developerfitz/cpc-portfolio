ad94a60f1543aa866f39dfeff3aab024dea2140c
developerfitz <fitz3364@gmail.com>
2020-09-28 01:04:16 -0400

    ♻️_refactor(frontend): api calls

--- netlify/index.js
+++ netlify/index.js
@@ -1,111 +1,90 @@
-console.log("Hello Examiners")
+// Lambda Urls
+BASE_URL = 'https://7tmyw05dri.execute-api.us-east-1.amazonaws.com/dev'
+UPLOAD_PORTFOLIO = `${BASE_URL}/examiner-portfolio`
+PROCESS_DOWNLOAD = `${BASE_URL}/get-processed-excel-sheet`
+
 
 function load(){
   const uploadButton = document.getElementById('#upload_button')
   const responseDiv = document.getElementById('#response')
 
-  if (uploadButton) {
-    const data = {name: 'good data'}
+  async function onClickHandler(e) {
+    e.preventDefault()
+    // Button to upload and capture the file
+    const uploadInput = document.getElementById('#upload_input')
+    // ! Assuming there is only one file being uploaded
+    const file = uploadInput.files[0]
 
-    uploadButton.addEventListener('click', (e) => {
-      const uploadInput = document.getElementById('#upload_input')
-      let BUCKET = ''
-      let processedFileKey = ''
-      // let getUploadeFileUrl = ''
-      let getProcessedFileUrl = ''
-
-      responseDiv.innerText = 'Loading...'
-
-
-      for (let file of uploadInput.files) {
-        console.log(file.name, file.type, file.size)
-        console.log(file)
-        const getTest = async () => {
-          const uploadUrl = 'https://7tmyw05dri.execute-api.us-east-1.amazonaws.com/dev/examiner-portfolio'
-          const downloadUrl = 'https://7tmyw05dri.execute-api.us-east-1.amazonaws.com/dev/get-processed-excel-sheet'
-
-          // This should probably just be a get for the presigned url
-          // not a put or uploading of the file
-          const options = {
-            method: 'PUT',
-            mode: 'cors',
-            headers: {
-              'Accept': file.type,
-              'Content-Type': file.type,
-              'X-filename': file.name,
-              'X-size': file.size,
-            },
-            body: file,
-          }
-          // PUT request for 🤷🏽‍♂️ but I get a presigned url
-          const req = new Request(uploadUrl, options)
-          const getPresignedUploadUrl = await fetch(req)
-            .then(resolve => {
-              const fulfilled = resolve.json()
-              console.log(resolve)
-              console.log(fulfilled)
-              return fulfilled 
-            }, rejected => new Error('Upload url not recieved'))
-            .then(async (data) => {
-              responseDiv.innerText = data.message
-              console.log(data)
-
-              // another request to upload file to S3 with presigned url
-              // TODO: ensure the upload happens before the next call
-              const putOptions = {
-                method: 'PUT',
-                mode: 'cors',
-                headers: {
-                  'Accept': file.type,
-                  'Content-Type': file.type,
-                  'X-filename': file.name,
-                  'X-size': file.size,
-                },
-                body: file,
-              }
-              const putReq = new Request(data.presignedUrl, putOptions)
-              const getPresignedDownloadUrl = await fetch(putReq)
-                .then(resolve => {
-                  console.log(resolve)
-                }, rejected => new Error('Download Url not recieved'))
-
-              // Start the file to be processed and downloaded
-              // TODO: ensure the file is uploaded before sending this request
-              const downloadFileOptions = {
-                method: 'PUT',
-                mode: 'cors',
-                body: JSON.stringify({
-                  filename: file.name,
-                  contentType: file.type,
-                  message: 'Local - can I have a url please?'
-                })
-              }
-              const getExcelFile = new Request(
-                downloadUrl, 
-                downloadFileOptions)
-              const startProcessingFile = await fetch(getExcelFile)
-                .then( resolve => {
-                  return resolve.json()
-                })
-                .then( data => {
-                  console.log(data)
-                  responseDiv.innerText = `
-                    Your file ${data.filename} has been downloaded. 
-                    Thank you for waiting. 
-                    ${data.message}.
-                    `
-                  window.open(data.presignedUrl, '_blank')
-                })
-                .catch( error => {
-                  console.warn(error)
-                  return new Error('File not processed.')
-                })
-
-            })
-        }  
-        getTest()
-      }
+    // Get a presignedUrl to upload file to S3 prior to processing
+    const presignedUrlRequest = new Request(UPLOAD_PORTFOLIO, {
+      method: 'PUT',
+      mode: 'cors',
+      headers: {
+        'Accept': file.type,
+        'Content-Type': file.type,
+        'X-filename': file.name,
+        'X-size': file.size,
+      },
+      body: file.file
     })
+    const response = await fetch(presignedUrlRequest)
+      .then( 
+        resolve => resolve.json(), 
+        reject => `No url given --> ${reject}`
+      )
+
+    // Notify user of progress
+    responseDiv.innerText = response.message
+
+    // Upload file using the presignedUrl
+    const uploadRequest = new Request(response.presignedUrl, {
+        method: 'PUT',
+        mode: 'cors',
+        headers: {
+          'Accept': file.type,
+          'Content-Type': file.type,
+          'X-filename': file.name,
+          'X-size': file.size,
+        },
+        body: file,
+      })
+    const uploadResponse = await fetch(uploadRequest)
+      .then(
+        resolve => { 
+          console.log(`File ${file.name} uploaded.`)
+          console.log(resolve.status)
+        },
+        reject => `Not uploaded --> ${reject}`
+      )
+    
+    // Call Lambda to process uploaded file and then download from S3
+    const processFileRequest = new Request(PROCESS_DOWNLOAD, {
+      method: 'PUT',
+      mode: 'cors',
+      body: JSON.stringify({
+        filename: file.name,
+        contentType: file.type,
+        message: 'Request - please process and send the processed file.'
+      })
+    })
+    const processFileResponse = await fetch(processFileRequest)
+      .then( 
+        resolve => resolve.json(), 
+        reject => `No url given --> ${reject}`
+      )
+    
+    // Notify user and download file in separate tab
+    responseDiv.innerText = `
+      Your file ${processFileResponse.filename} has been downloaded. 
+      Thank you for waiting. 
+      ${processFileResponse.message}.
+    `
+    window.open(processFileResponse.presignedUrl, '_blank')
+
+  }
+
+  if (uploadButton) {
+    uploadButton.addEventListener('click', onClickHandler)
   }
 }
 
